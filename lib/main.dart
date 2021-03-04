1e16222a96dcbc6f79dc86bbf9da7b557d4d84a4
FlutLab.io <>
2021-03-04 14:47:23 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'my_app.dart';
 import 'themes/style.dart';
 
 void main() => runApp(HomePage());
@@ -13,29 +13,3 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp ({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(350.0),
-        child: AppBar(
-          shape: ContinuousRectangleBorder(
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(30.0),
-              bottomRight: Radius.circular(30.0),
-            ),
-          ),
-          flexibleSpace: Container(
-            decoration: BoxDecoration(
-              image: appLogo,
-            ),
-          ),
-        ),
-      ),
-      body: Text("Random Text"),
-    );
-  }
-}
---
+++ lib/my_app.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp ({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(350.0),
+        child: AppBar(
+          shape: ContinuousRectangleBorder(
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(30.0),
+              bottomRight: Radius.circular(30.0),
+            ),
+          ),
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              image: appLogo,
+            ),
+          ),
+        ),
+      ),
+      body: Text("Random Text"),
+    );
+  }
+}
