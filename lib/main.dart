d7983827549a11ad19d70491f3791fb49dbfc452
FlutLab.io <>
2021-03-04 13:42:18 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(400.0),
+        preferredSize: Size.fromHeight(350.0),
         child: AppBar(
           shape: ContinuousRectangleBorder(
             borderRadius: BorderRadius.only(
