dd240cf1bff405482d3a989967385030d02c34b6
FlutLab.io <>
2021-03-04 17:33:46 +0000

    Autosave

--- lib/pages/text_widget.dart
+++ lib/pages/text_widget.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class TextWidget extends StatelessWidget {
   const TextWidget ({Key key, this.text}) : super(key: key);
 
-  final String text?;
+  final String text;
 
   @override
   Widget build(BuildContext context) {
