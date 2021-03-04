7fed6805b056ab80f51956edc7cc6b9d50ff0559
FlutLab.io <>
2021-03-04 13:24:13 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -3,5 +3,19 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        shape: ContinuousRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(18.0),
+            bottomRight: Radius.circular(18.0),
+          ),
+        ),
+        title: Center(child: Text("Sushi App")),
+        body: Text("Random Text"),
+      ),
+    );
+  }
 }
