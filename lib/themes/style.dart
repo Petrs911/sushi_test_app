98c9e829766af991263279e4e1fd40b37428e536
FlutLab.io <>
2021-03-04 14:36:44 +0000

    Autosave

--- lib/themes/style.dart
+++ lib/themes/style.dart
@@ -2,5 +2,6 @@ import 'package:flutter/material.dart';
 
 Image appLogo = Image(
   image: ExactAssetImage("assets/Screenshot_1.png"),
-  fit: BoxFit.cover,
+  height: double.infinity,
+  width: double.infinity,
 );
