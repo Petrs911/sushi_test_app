ecf57acc3afc2e246e3d1096fc9e07b21cbfdcc1
FlutLab.io <>
2021-03-04 13:32:19 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(HomePage());
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyApp(),
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   const MyApp ({Key key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
