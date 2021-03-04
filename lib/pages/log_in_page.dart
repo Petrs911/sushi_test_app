17bf529033507f738d65b2daaf008d05e4321211
FlutLab.io <>
2021-03-04 16:37:10 +0000

    Autosave

--- lib/pages/log_in_page.dart
+++ lib/pages/log_in_page.dart
@@ -47,7 +47,7 @@ class _LogInPage extends State<LogInPage> {
               hintText: 'madinasalik@yandex.ru', 
               hintStyle: TextStyle(
                 decoration: TextDecoration.underline,
-                decorationStyle: DecorationStyle.double,
+                decorationStyle: TextDecorationStyle.double,
                 color: Colors.black,
                 fontWeight: FontWeight.w600,
                 fontSize: 17.0,
