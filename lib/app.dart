998e3d7eb731babebd489f3b3b5f10f832a92143
FlutLab.io <>
2021-03-05 14:28:11 +0000

    Autosave

--- lib/app.dart
+++ lib/app.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
               ),
             ),
             bottom: TabBar(
-              filled: false,
+              indicatorSize: TabBarIndicatorSize(2),
               tabs: _Tabs,
               indicatorColor: Colors.red,
               indicatorWeight: 3.0,
