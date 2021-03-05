72dfe363fc17d1c661d1a49b2c900cc88b6fb3bb
FlutLab.io <>
2021-03-05 11:03:05 +0000

    Autosave

--- lib/pages/input_field_widget.dart
+++ lib/pages/input_field_widget.dart
@@ -30,7 +30,7 @@ class _InputFieldWidgetState extends State<InputFieldWidget> {
           focusedBorder: UnderlineInputBorder(
             borderSide: BorderSide(color: Colors.grey),
           ),
-          hintText: text,//'madinasalik@yandex.ru',
+          hintText: widget.text,//'madinasalik@yandex.ru',
           hintStyle: TextStyle(
             decoration: TextDecoration.underline,
             decorationStyle: TextDecorationStyle.double,
