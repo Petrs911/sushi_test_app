a2063c6c26ac61a8543210648b6ebfe4cce55815
FlutLab.io <>
2021-03-05 10:45:05 +0000

    Autosave

---
+++ lib/pages/password_field_widget.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class PasswordFieldWidget extends StatefulWidget {
+  const PasswordFieldWidget ({this.hintText});
+
+  final String hintText;
+
+  @override
+  _PasswordFieldWidgetState createState() => _PasswordFieldWidgetState();
+}
+
+class _PasswordFieldWidgetState extends PasswordFieldWidgetState {
+  bool _obscureText = true;
+
+  @override
+  Widget build (BuildContext context) {
+    return TextFormField(
+      obscureText: _obscureText,
+      maxLength: 8,
+      decoration: Input
+    );
+  }
+}
