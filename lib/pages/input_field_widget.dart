d726c457175c219150840ad930a50fa831e6c3c3
FlutLab.io <>
2021-03-05 10:09:43 +0000

    Autosave

--- lib/pages/input_field_widget.dart
+++ lib/pages/input_field_widget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/matrial.dart';
 
 class InputFieldWidget extends StatefulWidget {
-  const InputFieldWidget({Key key}) super(key: key)
+  const InputFieldWidget({Key key}) super(key: key);
   
   @override
   <StatefulWidget> createState() => _InputFieldWidget();
