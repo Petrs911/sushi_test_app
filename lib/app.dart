2aa7f0b4d8bc1490284825dee1ce282631771779
FlutLab.io <>
2021-03-05 14:18:39 +0000

    Autosave

--- lib/app.dart
+++ lib/app.dart
@@ -18,12 +18,12 @@ class MyApp extends StatelessWidget {
         child: Text('Войти', 
           style: TextStyle(color: Colors.black,
           fontSize: 18.0, 
-          fontWeight: FontWeight.w600))),
+          fontWeight: FontWeight.w700))),
       Tab(
         child: Text('Зарегистрироваться',
           style: TextStyle(color: Colors.black,
           fontSize: 18.0, 
-          fontWeight: FontWeight.w600))),
+          fontWeight: FontWeight.w700))),
     ];
 
     return DefaultTabController(
