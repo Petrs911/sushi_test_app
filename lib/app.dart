90e33088c500f24df47d34ac2fe0650f3bf95cd7
FlutLab.io <>
2021-03-05 15:29:51 +0000

    Autosave

--- lib/app.dart
+++ lib/app.dart
@@ -33,8 +33,8 @@ class MyApp extends StatelessWidget {
           child: AppBar(
             shape: ContinuousRectangleBorder(
               borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(60.0),
-                bottomRight: Radius.circular(60.0),
+                bottomLeft: Radius.circular(80.0),
+                bottomRight: Radius.circular(80.0),
               ),
             ),
             bottom: TabBar(
