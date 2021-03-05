077a0681b54c30cdf43de79acb1d3a6119f97952
FlutLab.io <>
2021-03-05 12:14:44 +0000

    Autosave

--- lib/pages/log_in_page.dart
+++ lib/pages/log_in_page.dart
@@ -16,7 +16,7 @@ class _LogInPage extends State<LogInPage> {
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
         SizedBox(height: 50.0),
         TextWidget(text: 'Адрес электронной почты', fontWeight: FontWeight.w900, fontSize: 17.0),
