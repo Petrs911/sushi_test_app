d41e9487751d4153871e42e5b8877f8d4bacca9b
FlutLab.io <>
2021-03-05 14:45:48 +0000

    Autosave

--- lib/app.dart
+++ lib/app.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
           fontSize: 18.0, 
           fontWeight: FontWeight.w700)))),
       Container(
-          width: 80.0,
+          width: 90.0,
           child: Tab(child: Text('Зарегистрироваться', 
             style: TextStyle(color: Colors.black,
             fontSize: 18.0, 
